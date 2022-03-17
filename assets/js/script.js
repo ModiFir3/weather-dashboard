ca899780ccdf99efa9d5e77ffef0f5ff6291ff1b
PaulS <psoliz333@gmail.com>
2022-03-17 01:02:02 -0500

    added 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var apiKey = "f31a9d61dacf5cd49a41c23a8bd6acbf";
 $("#userSubmit").on("click", function () {
     var location = $("#userInput").val();
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&daily.uvi&appid=" + apiKey;
-    
+
     //fetch openweather api
     fetch(apiUrl)
         .then(function (response) {
@@ -15,38 +15,45 @@ $("#userSubmit").on("click", function () {
                     var lat = data.coord.lat;
                     var lon = data.coord.lon;
 
-                    var oneCallApi = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon='+ lon +'&appid=' + apiKey
-                    console.log(data);
-                    console.log(lat);
-                    console.log(lon);
+                    var oneCallApi = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey
                     //save location to local storage
                     localStorage.setItem(locationId, location);
 
                     //daily weather
                     $('#cityname').text(data.name)
+                    $('#weathericon').attr('src', 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png');
                     $('#temp').text(data.main.temp + 'F');
                     $('#wind').text(data.wind.speed + 'MPH');
                     $('#humidity').text(data.main.humidity + '%');
 
                     //uv data
-                    fetch(oneCallApi).then(function(resp) {
-                        resp.json().then(function(data) {
-                            console.log(data.daily[0].uvi);
-                            console.log(data.daily[0]);
+                    fetch(oneCallApi).then(function (resp) {
+                        resp.json().then(function (data) {
+
+                            console.log(data);
+
                             //color index for UV 
                             if (data.daily[0].uvi < 2) {
                                 $('#UV-Index').text(data.daily[0].uvi).removeClass().addClass('bg-success rounded')
-                            } else if (data.daily[0].uvi > 2 && data.daily[0].uvi <= 5) {
+                            } else if (data.daily[0].uvi > 3 && data.daily[0].uvi < 6) {
                                 $('#UV-Index').text(data.daily[0].uvi).removeClass().addClass('bg-primary rounded')
-                            } else if (data.daily[0].uvi > 5 && data.daily[0].uvi <= 7 ) {
+                            } else if (data.daily[0].uvi > 6 && data.daily[0].uvi < 8) {
                                 $('#UV-Index').text(data.daily[0].uvi).removeClass().addClass('bg-warning rounded')
                             } else {
                                 $('#UV-Index').text(data.daily[0].uvi).removeClass().addClass('bg-danger rounded')
                             }
-                            
+
+                            //5 day forcast
+                            for (i = 0; i < data.daily.length; i++) {
+                                $('.forcast-icon').eq(i).attr('src', 'http://openweathermap.org/img/wn/'+ data.daily[i].weather[0].icon + '.png');
+                                $('.forcast-temp').eq(i).text(data.daily[i].temp.day + 'F');
+                                $('.forcast-wind').eq(i).text(data.daily[i].wind_speed + 'MPH');
+                                $('.forcast-humidity').eq(i).text(data.daily[i].humidity + '%');
+                            }
+
                         });
                     });
-                    
+
                 });
             } else {
                 //invalid input
